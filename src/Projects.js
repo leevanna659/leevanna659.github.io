bfc571da5329161280ef11d3afcc5d1cb34c2b39
Vanna <leevanna659@gmail.com>
2026-08-14 11:52:54 -0400

    edit description

--- src/Projects.js
+++ src/Projects.js
@@ -9,7 +9,7 @@ const Projects = () => {
              <a href="https://food-map-stack.onrender.com/" target="_blank">  <h3>Food Map Project</h3></a> 
                 <div className="project-content">
                 <img src="/food-mappic.png" className="project-image"></img>
-                <p>This is an app where you can view iconic or important foods in the US. By clicking on either the food from the list or marker on the map you can learn a little bit of information on it as well as where the states are on the map. Dishes may take a moment to load. Made with help from Copilot AI.</p>
+                <p>This is an app where you can view iconic or important foods in the US. By clicking on either the food from the list or marker on the map you can learn a little bit of information on it as well as where the states are on the map. Dishes may take a moment to load. Made with help from Copilot.</p>
                 </div>
                 </div>
             
