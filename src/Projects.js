7622b312fa15ac610828083c5f8c69e27e79eb3e
Vanna <leevanna659@gmail.com>
2026-08-14 09:15:10 -0400

    add project 1

--- src/Projects.js
+++ src/Projects.js
@@ -6,10 +6,10 @@ const Projects = () => {
 
 
             <div className="project-card card">
-                <h3>Project 1</h3>
+             <a href="https://food-map-stack.onrender.com/" target="_blank">  <h3>Food Map Project</h3></a> 
                 <div className="project-content">
-                <img src="/white.png" className="project-image"></img>
-                <p>No project yet</p>
+                <img src="/food-mappic.png" className="project-image"></img>
+                <p>This is a app where you can view iconic or important foods in the US. By clicking on either the food from the list or marker on the map you can learn a little bit of information on it as well as where the states are on the map. </p>
                 </div>
                 </div>
             
